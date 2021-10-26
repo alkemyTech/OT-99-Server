97c1cd0cea11c64ae6b3f62a1704e63a0e4e1c35
Tomas.Fogwill <tomasfogwill@gmail.com>
2021-10-26 14:23:56 -0300

    added activity repository

---
+++ src/main/java/com/alkemy/ong/repository/ActivityRepository.java
@@ -0,0 +1,9 @@
+package com.alkemy.ong.repository;
+
+import com.alkemy.ong.model.Activity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ActivityRepository extends JpaRepository<Activity,Long>{
+
+}
