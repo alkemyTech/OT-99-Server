17565929518fa8f2fea70623369f23351614b7ab
Santiago <delamersantiago@gmail.com>
2021-10-26 10:24:17 -0300

    OT99-18 add Category model and add Repository

---
+++ src/main/java/com/alkemy/ong/model/Category.java
@@ -0,0 +1,42 @@
+package com.alkemy.ong.model;
+
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+import org.springframework.boot.context.properties.bind.Name;
+import org.springframework.data.jpa.domain.AbstractPersistable;
+
+@Entity
+@Table(name = "categories")
+@SQLDelete(sql = "UPDATE categories SET DELETED = TRUE where id=?")
+@Where(clause = "deleted=false")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category extends AbstractPersistable<Long> {
+
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "description", nullable = true)
+    private String description;
+    @Column(name = "image", nullable = true)
+    private String image;
+    @Column(name = "deleted")
+    private Boolean deleted;
+    @Column(name = "creation_date")
+    private Timestamp creationDate;
+    @Column(name = "updated_at")
+    private Timestamp updatedAt;
+
+    public Category(Boolean isDeleted) {
+        this.deleted = false;
+    }
+
+}
---
+++ src/main/java/com/alkemy/ong/repository/CategoryRepository.java
@@ -0,0 +1,13 @@
+
+package com.alkemy.ong.repository;
+
+import com.alkemy.ong.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author delam
+ */
+public class CategoryRepository implements JpaRepository<Category , Long> {
+
+}
