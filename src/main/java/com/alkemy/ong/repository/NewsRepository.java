790603bc7c1542c71bb766846a0690674c6195da
Pablo Amado <pabloamado8@gmail.com>
2021-10-26 18:03:35 -0300

    newsRepository: annotation repository added

--- src/main/java/com/alkemy/ong/repository/NewsRepository.java
+++ src/main/java/com/alkemy/ong/repository/NewsRepository.java
@@ -1,9 +1,11 @@
 package com.alkemy.ong.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.alkemy.ong.model.News;
 
+@Repository
 public interface NewsRepository extends JpaRepository<News, Long>{
 
 }
