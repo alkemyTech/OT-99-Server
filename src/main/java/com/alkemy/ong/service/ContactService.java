1af3fbc7e27e56ab66cc5eef3d71d7a07b3ff88d
ingrid-inga <leidyingrid20@gmail.com>
2021-10-31 19:28:49 -0300

    updated contact service according request

--- src/main/java/com/alkemy/ong/service/ContactService.java
+++ src/main/java/com/alkemy/ong/service/ContactService.java
@@ -1,35 +1,9 @@
 package com.alkemy.ong.service;
 
-import java.util.*;
-
-import com.alkemy.ong.model.Contact;
-import com.alkemy.ong.repository.ContactRepository;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-
 @Service
-@AllArgsConstructor
-@NoArgsConstructor
-
 public class ContactService {
-
-@Autowired
-ContactRepository repository;
-
-
-
-    public void createContact(Contact contact) {
-        repository.save(contact);
-    }
-
-
-
-
-
 
 
 }
