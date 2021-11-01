f49e7550d734694cf66dcee8512b0805d50ad720
Tomas.Fogwill <tomasfogwill@gmail.com>
2021-11-01 16:31:26 -0300

    remove @table annotation, changed table name to organization

--- src/main/java/com/alkemy/ong/model/Organization.java
+++ src/main/java/com/alkemy/ong/model/Organization.java
@@ -17,7 +17,6 @@ import org.hibernate.annotations.Where;
 import lombok.Getter;
 import lombok.Setter;
 
-@Table(name = "organizations")
 @Entity
 @SQLDelete(sql = "UPDATE organizations SET is_deleted=true WHERE id=?")
 @Where(clause = "is_deleted=false")
