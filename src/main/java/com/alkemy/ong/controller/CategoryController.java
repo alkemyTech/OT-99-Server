21e0fb01be6f63034154def9431865e5c389a584
Pablo Amado <pabloamado8@gmail.com>
2021-11-04 13:04:42 -0300

    OT99-40-CategoryController-GetAllEndpoint: class CategoryController added, fnction getAllCategories added

---
+++ src/main/java/com/alkemy/ong/controller/CategoryController.java
@@ -0,0 +1,30 @@
+package com.alkemy.ong.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alkemy.ong.dto.CategoryDtoGetAll;
+import com.alkemy.ong.service.CategoryService;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryController {
+
+	@Autowired
+	private CategoryService categoryService;
+	
+	@GetMapping
+	public ResponseEntity<List<CategoryDtoGetAll>> getAllCategories(){
+		
+		List<CategoryDtoGetAll> categories=categoryService.getAllCategories();
+				
+		return ResponseEntity.status(HttpStatus.OK).body(categories);
+	}
+
+}
