d1c8e84eed73bb84d3cb8edada422406b4e691d9
Pablo Amado <pabloamado8@gmail.com>
2021-11-04 00:24:34 -0300

    templateMessages added standar messages and methods

--- src/main/java/com/alkemy/ong/email/EmailTemplateMessage.java
+++ src/main/java/com/alkemy/ong/email/EmailTemplateMessage.java
@@ -1,5 +1,48 @@
 package com.alkemy.ong.email;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class EmailTemplateMessage {
 
+private  final String REGISTER_TITTLE="Registro de Cuenta:";
+	
+	private  final String ACTIVITY_TITTLE="Actividad Detectada:";
+	
+	private  final String WELCOME_SUBJECT="Bienvenido a Somos Mas!";
+	
+	private  final String ACTIVITY_SUBJECT="Alerta de Actividad.";
+	
+	private  final String CONTACT_MAIL="Mail: somosfundacionmas@gmail.com";
+			
+	private  final String CONTACT_CELL="Teléfono de contacto: 1160112988";
+	
+	public String getRegisterTittle() {
+		return REGISTER_TITTLE;
+	}
+
+	public String getActivityTittle() {
+		return ACTIVITY_TITTLE;
+	}
+
+	public String getWelcomeSubject() {
+		return WELCOME_SUBJECT;
+	}
+
+	public String getActivitySubject() {
+		return ACTIVITY_SUBJECT;
+	}
+
+	public String getContactMail() {
+		return CONTACT_MAIL;
+	}
+	
+	public String getContactCell() {
+		return CONTACT_CELL;
+	}
+	
+	public String getWelcomeBody(String username) {
+		
+		return "Bienvenido/a " + username + " gracias por haberte registrado en nuestro sitio web.";
+	}
 }
