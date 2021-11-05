0dc324e094bd31fc831e34f59718fb31490e2735
Santiago <delamersantiago@gmail.com>
2021-11-05 17:45:32 -0300

    Switch back changes on userRepository

--- src/main/java/com/alkemy/ong/repository/UserRepository.java
+++ src/main/java/com/alkemy/ong/repository/UserRepository.java
@@ -1,8 +1,6 @@
 package com.alkemy.ong.repository;
 
 
-
-
 import com.alkemy.ong.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,4 +9,5 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<Users, Long>{
 
     Users findByEmail(String email);
+
 }
