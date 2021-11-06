bedaee860a901f6d8a5b95d7fe14f6c54d26fda6
Pablo Amado <pabloamado8@gmail.com>
2021-11-05 22:58:41 -0300

    OT99-29-MailTemplate: conflict resolved by merging Ot99 27 integrate email service

--- src/main/java/com/alkemy/ong/service/EmailService.java
+++ src/main/java/com/alkemy/ong/service/EmailService.java
@@ -1,10 +1,14 @@
 package com.alkemy.ong.service;
 
+import java.io.IOException;
+import com.alkemy.ong.dto.UserRegisterResponse;
 import com.sendgrid.Response;
 import com.sendgrid.helpers.mail.objects.Content;
 
 public interface EmailService {
 
   Response sendEmail(String to, String subject, Content content);
+
+  Response sendWelcomeEmail(UserRegisterResponse user) throws IOException;
 
 }
--- src/main/java/com/alkemy/ong/service/impl/EmailServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/EmailServiceImpl.java
@@ -1,7 +1,6 @@
 package com.alkemy.ong.service.impl;
 
 import java.io.IOException;
-
 import com.alkemy.ong.config.SendGridConfig;
 import com.alkemy.ong.service.EmailService;
 import com.sendgrid.Method;
@@ -11,21 +10,28 @@ import com.sendgrid.SendGrid;
 import com.sendgrid.helpers.mail.Mail;
 import com.sendgrid.helpers.mail.objects.Content;
 import com.sendgrid.helpers.mail.objects.Email;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import com.alkemy.ong.dto.UserRegisterResponse;
+import com.alkemy.ong.email.MailMessage;
 
 @Service
 public class EmailServiceImpl implements EmailService {
 
     private final String endpoint = "mail/send";
+    private static final String TEXT_HTML="text/html";
 
     @Value("${sendgrid.sender.email}")
     private String senderEmail;
 
     @Autowired
     SendGridConfig sendGridConfig;
+    
+	@Autowired
+	private TemplateEngine templateEngine;
 
     @Override
     public Response sendEmail(String email, String subject, Content content) {
@@ -46,5 +52,30 @@ public class EmailServiceImpl implements EmailService {
         return response;
 
     }
+
+	@Override
+	public Response sendWelcomeEmail(UserRegisterResponse user) throws IOException {
+		
+		String mailBody=preparedWelcomeBodyEmail(user);
+		
+		Content content= new Content(TEXT_HTML,mailBody);
+		
+		Response response=sendEmail(user.getEmail(),MailMessage.WELCOME_SUBJECT.getValue(),content);
+		
+		return response;
+		
+	}
+	
+	private String preparedWelcomeBodyEmail(UserRegisterResponse user) {
+		
+		Context context=new Context();
+		context.setVariable("tittleContent", MailMessage.REGISTER_TITTLE.getValue());
+		context.setVariable("textContent", MailMessage.getWelcomeMsg(user.getFirstName(), user.getLastName() ));
+		context.setVariable("mailContact", MailMessage.CONTACT_MAIL.getValue());
+		context.setVariable("cellContact", MailMessage.CONTACT_CELL.getValue());
+		
+		return templateEngine.process("MailTemplate.html", context);
+		
+	}
 
 }
