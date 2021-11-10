c7c57ef9afd08e5aec53a9485bb72075fd2a009a
Pablo Amado <pabloamado8@gmail.com>
2021-11-09 23:12:20 -0300

    OT99-56-Slide-get-Endpoint: added method in ErrorHandler for EntityNotFoundException (auto throw method in repository)

--- src/main/java/com/alkemy/ong/exception/ErrorHandler.java
+++ src/main/java/com/alkemy/ong/exception/ErrorHandler.java
@@ -1,14 +1,18 @@
 package com.alkemy.ong.exception;
 
+import javax.persistence.EntityNotFoundException;
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class ErrorHandler {
+public class ErrorHandler  extends ResponseEntityExceptionHandler{
 
     @ExceptionHandler(EmailAlreadyExistException.class)
     public ResponseEntity<?> handleEmailAlreadyExist(HttpServletRequest request, EmailAlreadyExistException e) {
@@ -23,4 +27,11 @@ public class ErrorHandler {
         return new ResponseEntity<>(e.getMessage(),HttpStatus.FORBIDDEN);
     }
 
+    @ExceptionHandler(value= EntityNotFoundException.class)
+	protected ResponseEntity<?> nombreDelMetodo(EntityNotFoundException e, WebRequest webRequest){
+		
+		String message= "The entity does not exists:" + e.getMessage();
+		
+		return handleExceptionInternal(e, message, new HttpHeaders(), HttpStatus.NOT_FOUND, webRequest);
+	}
 }
