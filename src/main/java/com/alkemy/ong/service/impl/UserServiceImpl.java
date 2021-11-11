695a8f77b6b2094b81377ef523d38226222cac85
Pablo Amado <pabloamado8@gmail.com>
2021-11-11 19:02:48 -0300

    method findByEmail in UserServiceImpl changed, added the recovery of the role because the lazy fetchtype

--- src/main/java/com/alkemy/ong/service/impl/UserServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/UserServiceImpl.java
@@ -18,6 +18,8 @@ import com.alkemy.ong.service.RoleService;
 import com.alkemy.ong.service.UserService;
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -48,8 +50,13 @@ public class UserServiceImpl implements UserService {
     private AuthenticationManager authenticationManager;
 
     @Override
+    @Transactional
     public Users findByEmail(String email) {
-        return userRepo.findByEmail(email);
+        Users user=userRepo.findByEmail(email);
+        
+        user.getRole().getName();
+        
+        return user;
     }
 
     @Override
