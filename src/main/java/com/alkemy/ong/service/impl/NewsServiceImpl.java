57cddfaabd37e499036ce3a9d2aae9f986db0402
Pablo Amado <pabloamado8@gmail.com>
2021-11-13 20:05:16 -0300

    OT99-46-47-Creation-Update-News: save and update methods implemented

--- src/main/java/com/alkemy/ong/service/impl/NewsServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/NewsServiceImpl.java
@@ -1,20 +1,59 @@
 package com.alkemy.ong.service.impl;
 
+import com.alkemy.ong.dto.NewsDtoPersist;
 import com.alkemy.ong.dto.NewsDto;
 import com.alkemy.ong.exception.NotFoundException;
+import com.alkemy.ong.mapper.NewsMapper;
+import com.alkemy.ong.model.Category;
 import com.alkemy.ong.model.News;
 import com.alkemy.ong.repository.NewsRepository;
+import com.alkemy.ong.service.CategoryService;
 import com.alkemy.ong.service.NewsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NewsServiceImpl implements NewsService {
-    @Autowired
-    private NewsRepository repo;
-    @Override
-    public NewsDto getById(Long id) throws NotFoundException {
-        News news = repo.findById(id).orElseThrow(() -> new NotFoundException("The news is not registered."));
-        return NewsDto.mapToDto(news);
-    }
+
+	@Autowired
+	private NewsRepository newsRepository;
+	
+	@Autowired
+	private CategoryService categoryService;
+	
+	@Autowired
+	private NewsMapper newsMapper;
+	
+	@Override
+	public NewsDto save(NewsDtoPersist newsDto) throws NotFoundException {
+		
+		Category category=categoryService.getCategoryById(newsDto.getCategoryId());
+		
+		News news=newsRepository.save(newsMapper.toNews(newsDto, category));
+		
+		return newsMapper.toNewsDto(news);
+	}
+
+	@Override
+	public NewsDto update(NewsDtoPersist newsDto, Long id) throws NotFoundException {
+		
+		News news=newsRepository.getById(id);
+		
+		Category category=categoryService.getCategoryById(newsDto.getCategoryId());
+		
+		newsMapper.updateNews(news,newsDto, category);
+		
+		news=newsRepository.save(news);
+		
+		return newsMapper.toNewsDto(news);
+	}
+
+	@Override
+	public NewsDto getById(Long id) throws NotFoundException {
+
+		News news = newsRepository.findById(id).orElseThrow(() -> new NotFoundException("The news is not registered."));
+
+		return newsMapper.toNewsDto(news);
+	}
+	
 }
