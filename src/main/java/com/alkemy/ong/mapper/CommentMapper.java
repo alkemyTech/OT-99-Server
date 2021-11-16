ec4553892724ee4548a4d57a6bcc8122048abe43
Pablo Amado <pabloamado8@gmail.com>
2021-11-16 11:11:30 -0300

    OT99-75-CommentList-Endpoint: annotation component added

--- src/main/java/com/alkemy/ong/mapper/CommentMapper.java
+++ src/main/java/com/alkemy/ong/mapper/CommentMapper.java
@@ -4,9 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.stereotype.Component;
+
 import com.alkemy.ong.dto.CommentDto;
 import com.alkemy.ong.model.Comment;
 
+@Component
 public class CommentMapper {
 
 	public List<CommentDto> toCommentDtoList(List<Comment> comments) {
