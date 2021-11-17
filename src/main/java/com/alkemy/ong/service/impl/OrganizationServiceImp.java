1e6e0c9dceb7c6e2f55e7e85a6b1d853a106d409
Pablo Amado <pabloamado8@gmail.com>
2021-11-17 00:25:21 -0300

    OT99-60-GetSlides-ByOrganization: method getOrganizationDetails modified, now recover the organization and its slides

--- src/main/java/com/alkemy/ong/service/impl/OrganizationServiceImp.java
+++ src/main/java/com/alkemy/ong/service/impl/OrganizationServiceImp.java
@@ -1,44 +1,52 @@
 package com.alkemy.ong.service.impl;
 
 import java.util.List;
-import java.util.stream.Collectors;
-
 import com.alkemy.ong.dto.OrganizationDetailsResponse;
 import com.alkemy.ong.dto.OrganizationRequest;
+import com.alkemy.ong.dto.SlideDtoGet;
 import com.alkemy.ong.mapper.OrganizationMapper;
 import com.alkemy.ong.model.Organization;
 import com.alkemy.ong.repository.OrganizationRepository;
 import com.alkemy.ong.service.OrganizationService;
+import com.alkemy.ong.service.SlideService;
 import java.io.IOException;
 import java.time.LocalDateTime;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrganizationServiceImp implements OrganizationService {
 
-    @Autowired
-    OrganizationRepository organizationRepo;
-    @Autowired
-    OrganizationMapper organizationMapper;
+	@Autowired
+	OrganizationRepository organizationRepository;
 
-    @Override
-    public List<OrganizationDetailsResponse> getOrganizationDetails() {
+	@Autowired
+	SlideService slideService;
 
-        List<OrganizationDetailsResponse> organizations = organizationRepo.findAll().stream()
-                .map(OrganizationDetailsResponse::mapToResponse).collect(Collectors.toList());
-        return organizations;
-    }
+	@Autowired
+	OrganizationMapper organizationMapper;
 
-    @Override
-    public Organization registerOrganization(OrganizationRequest orgRequest) throws IOException {
-        orgRequest.setUpdateDateTime(LocalDateTime.now());
-        orgRequest.setCreationDateTime(LocalDateTime.now());
-        Organization organization = organizationMapper.dtoToEntity(orgRequest);
-        organizationRepo.save(organization);
-        return organization;
-        
-    }
+	@Override
+	public OrganizationDetailsResponse getOrganizationDetails(Long id) {
+		
+		Organization organization = organizationRepository.getById(id);
+
+		List<SlideDtoGet> organizationSlides = slideService.getAllSlides(organization);
+
+		return organizationMapper.toOrganizationResponse(organization, organizationSlides);
+		
+	}
+	
+	@Override
+	public Organization registerOrganization(OrganizationRequest orgRequest) throws IOException {
+
+		orgRequest.setUpdateDateTime(LocalDateTime.now());
+		orgRequest.setCreationDateTime(LocalDateTime.now());
+		Organization organization = organizationMapper.dtoToEntity(orgRequest);
+		organizationRepository.save(organization);
+
+		return organization;
+
+	}
 
 }
