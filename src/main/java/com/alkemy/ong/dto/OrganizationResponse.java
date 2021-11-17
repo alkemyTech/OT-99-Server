c945f64a0cb9ed4d0a072b748c9e38a12da0d275
Pablo Amado <pabloamado8@gmail.com>
2021-11-17 00:44:57 -0300

    OT99-60-GetSlides-ByOrganization: name class changed

--- src/main/java/com/alkemy/ong/dto/OrganizationResponse.java
+++ src/main/java/com/alkemy/ong/dto/OrganizationResponse.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 
-public class OrganizationDetailsResponse {
+public class OrganizationResponse {
     
     private String name;
     private String image;
