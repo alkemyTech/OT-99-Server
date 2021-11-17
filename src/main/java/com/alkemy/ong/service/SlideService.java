677b75b3b857d336268fd2c7bb75350545968cc1
Pablo Amado <pabloamado8@gmail.com>
2021-11-17 00:26:54 -0300

    OT99-60-GetSlides-ByOrganization: method getAllSlides(Organization) added

--- src/main/java/com/alkemy/ong/service/SlideService.java
+++ src/main/java/com/alkemy/ong/service/SlideService.java
@@ -3,6 +3,7 @@ package com.alkemy.ong.service;
 import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import com.alkemy.ong.dto.SlideDtoGet;
+import com.alkemy.ong.model.Organization;
 import com.alkemy.ong.dto.SlideDto;
 
 public interface SlideService {
@@ -10,6 +11,8 @@ public interface SlideService {
     public void deleteSlide(Long id) throws EntityNotFoundException;
 
     List<SlideDto> getAllSlides();
+    
+    List<SlideDtoGet> getAllSlides(Organization org);
     
 	SlideDtoGet getSlide(Long id);
 
