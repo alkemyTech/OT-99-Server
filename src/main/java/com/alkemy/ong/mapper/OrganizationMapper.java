36ec6874220e2280f769f88a4da7a52b7ced6be2
Pablo Amado <pabloamado8@gmail.com>
2021-11-17 00:37:30 -0300

    OT99-60-GetSlides-ByOrganization: method toOrganizationResponse added, parse an add dtoSlides

--- src/main/java/com/alkemy/ong/mapper/OrganizationMapper.java
+++ src/main/java/com/alkemy/ong/mapper/OrganizationMapper.java
@@ -1,23 +1,39 @@
-
 package com.alkemy.ong.mapper;
 
+import com.alkemy.ong.dto.OrganizationDetailsResponse;
 import com.alkemy.ong.dto.OrganizationRequest;
+import com.alkemy.ong.dto.SlideDtoGet;
 import com.alkemy.ong.model.Organization;
+import java.util.List;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OrganizationMapper {
-    
-@Autowired
-ModelMapper modelMapper;
 
-public Organization dtoToEntity(OrganizationRequest orgDto){
-    Organization organization = modelMapper.map(orgDto, Organization.class);
-    return organization;
-}
+	@Autowired
+	ModelMapper modelMapper;
 
+	public Organization dtoToEntity(OrganizationRequest orgDto) {
 
+		Organization organization = modelMapper.map(orgDto, Organization.class);
+
+		return organization;
+	}
+
+	public OrganizationDetailsResponse toOrganizationResponse(Organization organization,
+			List<SlideDtoGet> organizationSlides) {
+
+		OrganizationDetailsResponse organizationResponse = new OrganizationDetailsResponse();
+		organizationResponse.setName(organization.getName());
+		organizationResponse.setImage(organization.getImage());
+		organizationResponse.setPhone(organization.getPhone());
+		organizationResponse.setAdress(organization.getAddress());
+		organizationResponse.setOrganizationSlides(organizationSlides);
+		
+		return organizationResponse;
+
+	}
 
 }
