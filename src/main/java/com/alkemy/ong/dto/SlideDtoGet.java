963b5c7957ba2a1d6221c11c120ed8ec681f0575
Pablo Amado <pabloamado8@gmail.com>
2021-11-17 00:33:19 -0300

    OT99-60-GetSlides-ByOrganization: added necesary constructor used in SlideMapper

--- src/main/java/com/alkemy/ong/dto/SlideDtoGet.java
+++ src/main/java/com/alkemy/ong/dto/SlideDtoGet.java
@@ -1,11 +1,20 @@
 package com.alkemy.ong.dto;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class SlideDtoGet {
 
-	 private String imageUrl;
-	 private String text;
-	 private Integer slideOrder;
+	private String imageUrl;
+	private String text;
+	private Integer slideOrder;
+	
+	 public SlideDtoGet(String imageUrl, String text, Integer slideOrder) {
+		this.imageUrl=imageUrl;
+		this.text=text;
+		this.slideOrder=slideOrder;
+	}
+
 }
