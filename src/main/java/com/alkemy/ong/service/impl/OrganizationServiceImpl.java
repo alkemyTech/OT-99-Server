66d1fbc1c29b23787ca9f049b70c2e618f648cc4
Pablo Amado <pabloamado8@gmail.com>
2021-11-18 20:13:02 -0300

    OT99-60-GetSlides-ByOrganization: class OrganizationServiceImp renamed to OrganizationServiceImpl

--- src/main/java/com/alkemy/ong/service/impl/OrganizationServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/OrganizationServiceImpl.java
@@ -15,7 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class OrganizationServiceImp implements OrganizationService {
+public class OrganizationServiceImpl implements OrganizationService {
 
 	@Autowired
 	OrganizationRepository organizationRepository;
