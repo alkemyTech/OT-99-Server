caad219960e32e25437699612285977b81fc15af
Santiago <delamersantiago@gmail.com>
2021-11-19 17:57:50 -0300

    Add better format

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,25 @@
-REPLACE INTO organization(id,ORG_NAME, ORG_IMAGE_URL, ORG_ADDRESS, ORG_PHONE, ORG_EMAIL, ORG_WELCOME_TEXT, ORG_ABOUT_US_TEXT, IS_DELETED, LAST_UPDATE_DATE, CREATION_DATE )
- VALUES(1,'Somos Más',
+REPLACE INTO organization(id,
+ORG_NAME, 
+ORG_IMAGE_URL, 
+ORG_ADDRESS, 
+ORG_PHONE, 
+ORG_EMAIL, 
+ORG_WELCOME_TEXT, 
+ORG_ABOUT_US_TEXT, 
+IS_DELETED, 
+LAST_UPDATE_DATE, 
+CREATION_DATE )
+ VALUES(1,
+'Somos Más',
  'https://drive.google.com/drive/u/2/folders/1q-t5pTX3FA7bf-2kDnGq-99EUfkUQ7iZ',
  'La Cava, San Isidro, Provincia de Buenos Aires', 
  1160112988, 
  'somosfundacionmas@gmail.com',
  'Somos Más es una organización sin fines de lucro que trabaja desde 1997 junto a los vecinos del barrio La Cava para mejorar las vidas de aquellos en situación de vulnerabilidad, garantizando un proceso de crecimiento e inserción social.',
- 'Desde 1997 trabajamos con los chicos y chicas, mamás y papás, abuelos y vecinos del barrio La Cava para garantizar la mejora de la situación de aquellos en vulnerabilidad. Uniendo las manos de las familias tanto del barrio como fuera de él, hemos mejorado nuestra capacidad de trabajo y nuestra organización que inicialmente fue creada con la intención de dar alimento a aquellos que lo necesitaban, hoy es un centro comunitario que acompaña a más de 700 personas a través de las áreas de educación, deportes, primera infancia, salud, alimentación y trabajo social.
- Nuestra visión es mejorar la calidad de vida de niños y familias en situación de vulnerabilidad en el
-barrio La Cava, otorgando un cambio de rumbo en cada individuo a través de la
-educación, salud, trabajo, deporte, responsabilidad y compromiso.
+ 'Desde 1997 trabajamos con los chicos y chicas, mamás y papás, abuelos y vecinos del barrio La Cava para garantizar la mejora de la situación de aquellos en vulnerabilidad. 
+Uniendo las manos de las familias tanto del barrio como fuera de él, hemos mejorado nuestra capacidad de trabajo y nuestra organización que inicialmente fue creada con la intención de dar alimento a aquellos que lo necesitaban, 
+hoy es un centro comunitario que acompaña a más de 700 personas a través de las áreas de educación, deportes, primera infancia, salud, alimentación y trabajo social.
+ Nuestra visión es mejorar la calidad de vida de niños y familias en situación de vulnerabilidad en el barrio La Cava, otorgando un cambio de rumbo en cada individuo a través de la educación, salud, trabajo, deporte, responsabilidad y compromiso.
  Estamos comprometidos a trabajar articuladamente con los distintos aspectos de la vida de las familias,
 generando espacios de desarrollo personal y familiar, brindando herramientas que
 logren mejorar la calidad de vida a través de su propio esfuerzo.',
