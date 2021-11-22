968b2b806d753db4a35aeac5e9c98c88278af75d
Tomas.Fogwill <tomasfogwill@gmail.com>
2021-11-22 19:31:37 -0300

    addded endpoint to controller

--- src/main/java/com/alkemy/ong/controller/SlideController.java
+++ src/main/java/com/alkemy/ong/controller/SlideController.java
@@ -1,6 +1,9 @@
 package com.alkemy.ong.controller;
 
 import java.util.List;
+
+import javax.validation.Valid;
+
 import com.alkemy.ong.dto.SlideDto;
 import com.alkemy.ong.service.SlideService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,10 +11,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import com.alkemy.ong.dto.SlideDtoGet;
+import com.alkemy.ong.dto.SlideDtoUpdate;
+import com.alkemy.ong.exception.NotFoundException;
 
 @RestController
 @RequestMapping("/slides")
@@ -22,22 +29,28 @@ public class SlideController {
 
     @GetMapping
     public ResponseEntity<List<SlideDto>> getAllSlides() {
-    	
+
         return new ResponseEntity<>(slideService.getAllSlides(), HttpStatus.OK);
 
     }
-    
-	@GetMapping("/{id}")
-	public ResponseEntity<SlideDtoGet> getSlide(@PathVariable Long id){
-		
-		return new ResponseEntity<>(slideService.getSlide(id),HttpStatus.OK);
-	}
-  
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SlideDtoGet> getSlide(@PathVariable Long id) {
+
+        return new ResponseEntity<>(slideService.getSlide(id), HttpStatus.OK);
+    }
+
     @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable Long id) {
-    	
+
         slideService.deleteSlide(id);
-        
+
         return new ResponseEntity<>("Slide has been deleted", HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<SlideDtoUpdate> updateSlide(@PathVariable Long id,
+            @Valid @RequestBody SlideDtoUpdate slideDto) throws NotFoundException {
+        return new ResponseEntity<>(slideService.updateSlide(id, slideDto), HttpStatus.OK);
     }
 }
