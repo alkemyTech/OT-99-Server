0ad084c75ebb4c7b363cfcc2aae8e8d9d7925e86
Tomas.Fogwill <tomasfogwill@gmail.com>
2021-11-22 19:30:04 -0300

    created mapper SlideDtoUpdateToSlide

--- src/main/java/com/alkemy/ong/mapper/SlideMapper.java
+++ src/main/java/com/alkemy/ong/mapper/SlideMapper.java
@@ -3,18 +3,27 @@ package com.alkemy.ong.mapper;
 import org.springframework.stereotype.Component;
 
 import com.alkemy.ong.dto.SlideDtoGet;
+import com.alkemy.ong.dto.SlideDtoUpdate;
 import com.alkemy.ong.model.Slide;
 
 @Component
 public class SlideMapper {
 
 	public SlideDtoGet toSlideDtoGet(Slide slide) {
-		
-		SlideDtoGet slideDto=new SlideDtoGet();
+
+		SlideDtoGet slideDto = new SlideDtoGet();
 		slideDto.setImageUrl(slide.getImageUrl());
 		slideDto.setText(slide.getText());
 		slideDto.setSlideOrder(slide.getSlideOrder());
-		
+
 		return slideDto;
+	}
+
+	public Slide toSlide(SlideDtoUpdate slideDto) {
+		Slide slide = new Slide();
+		slide.setImageUrl(slideDto.getImageUrl());
+		slide.setText(slideDto.getText());
+		slide.setSlideOrder(slideDto.getSlideOrder());
+		return slide;
 	}
 }
