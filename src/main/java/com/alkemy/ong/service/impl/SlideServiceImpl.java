b054f83ba92d967ff852f7934b4220442aec443b
Santiago <delamersantiago@gmail.com>
2021-11-24 15:23:28 -0300

    OT99-HOTFIX Relocate converter to image Service imp

--- src/main/java/com/alkemy/ong/service/impl/SlideServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/SlideServiceImpl.java
@@ -43,7 +43,7 @@ public class SlideServiceImpl implements SlideService {
     @Override
     public Slide create(SlideDtoPost slideDtoPost) {
         Slide slide = slideMapper.dtoToEntity(slideDtoPost);
-        Base64DecodedMultipartFile file = this.convert(slide.getImageUrl());
+        Base64DecodedMultipartFile file = imageService.convert(slide.getImageUrl());
         String name = UUID.randomUUID().toString();
         String extension = ".jpg";
         file.setName(name + extension);
@@ -83,15 +83,6 @@ public class SlideServiceImpl implements SlideService {
     }
 
     @Override
-
-    public Base64DecodedMultipartFile convert(String image) {
-        byte[] result = Base64.getDecoder().decode(image);
-        Base64DecodedMultipartFile file = new Base64DecodedMultipartFile(result);
-        return file;
-
-    }
-
-    @Override
     public List<SlideDtoGet> getAllSlidesByOrganization(Organization org) {
 
         List<Slide> slides = slideRepository.findAllByOrganizationId(org);
@@ -99,6 +90,7 @@ public class SlideServiceImpl implements SlideService {
         return slideMapper.toSlideDtoGetList(slides);
     }
 
+    @Override
     public SlideDtoUpdate updateSlide(Long id, SlideDtoUpdate slideDto) throws NotFoundException {
         if (!slideRepository.existsById(id)) {
             throw new NotFoundException("Couldn't find slide with id : " + id);
