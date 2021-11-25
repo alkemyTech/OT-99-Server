6fd73036c0811d06706b79535ea5f54bf6ee07eb
Pablo Amado <pabloamado8@gmail.com>
2021-11-25 18:23:49 -0300

    OT99-83-TestimonialsPagination: method getPage added

--- src/main/java/com/alkemy/ong/controller/TestimonialController.java
+++ src/main/java/com/alkemy/ong/controller/TestimonialController.java
@@ -1,5 +1,6 @@
 package com.alkemy.ong.controller;
 
+import com.alkemy.ong.dto.TestimonialDto;
 import com.alkemy.ong.dto.TestimonialRequest;
 import com.alkemy.ong.exception.DataAlreadyExistException;
 import com.alkemy.ong.exception.NotFoundException;
@@ -7,6 +8,8 @@ import com.alkemy.ong.model.Testimonial;
 import com.alkemy.ong.service.TestimonialService;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.List;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
@@ -32,14 +36,23 @@ public class TestimonialController {
 
     @PutMapping("/{id}")
     public ResponseEntity<Testimonial> update(@Valid @RequestBody TestimonialRequest testimonialRequest, @PathVariable Long id) throws NotFoundException, DataAlreadyExistException {
-        return new ResponseEntity<>(testimonialService.updateTestimonial(id, testimonialRequest), HttpStatus.OK);
+       
+    	return new ResponseEntity<>(testimonialService.updateTestimonial(id, testimonialRequest), HttpStatus.OK);
 
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable Long id) throws NotFoundException {
-        testimonialService.delete(id);
-        return new ResponseEntity<>("Testimonial has been deleted", HttpStatus.OK);
+       
+    	testimonialService.delete(id);
+       
+    	return new ResponseEntity<>("Testimonial has been deleted", HttpStatus.OK);
+    }
+    
+    @GetMapping
+    public ResponseEntity<List<TestimonialDto>> getPage(@RequestParam (defaultValue = "0") Integer page ) throws NotFoundException{
+        
+        return new ResponseEntity<>(testimonialService.getPage(page), HttpStatus.OK);
     }
 
 }
