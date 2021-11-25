289ad0be5c6315ee90581d1a208288a8ce8bf8e6
Pablo Amado <pabloamado8@gmail.com>
2021-11-25 18:26:30 -0300

    OT99-83-TestimonialsPagination: method getPage that return a list of testimonialDto added

--- src/main/java/com/alkemy/ong/service/TestimonialService.java
+++ src/main/java/com/alkemy/ong/service/TestimonialService.java
@@ -5,10 +5,17 @@ import com.alkemy.ong.exception.DataAlreadyExistException;
 import com.alkemy.ong.exception.NotFoundException;
 import com.alkemy.ong.model.Testimonial;
 
+import java.util.List;
+
+import com.alkemy.ong.dto.TestimonialDto;
+
 public interface TestimonialService {
 
     public Testimonial save(TestimonialRequest testimonialRequest) throws DataAlreadyExistException;
+    
     public Testimonial updateTestimonial(Long id, TestimonialRequest testimonialRequest) throws NotFoundException, DataAlreadyExistException;
 
     public void delete(Long id) throws NotFoundException;
+    
+    public List<TestimonialDto> getPage (Integer page) throws NotFoundException;
 }
