0b157ac742af3afe89130307306f9babee15d539
Tomas.Fogwill <tomasfogwill@gmail.com>
2021-11-25 14:49:55 -0300

    added findAll method with pageable parameter and page return

--- src/main/java/com/alkemy/ong/repository/CategoryRepository.java
+++ src/main/java/com/alkemy/ong/repository/CategoryRepository.java
@@ -4,6 +4,9 @@ import java.util.Optional;
 
 import com.alkemy.ong.dto.CategoryDto;
 import com.alkemy.ong.model.Category;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -16,5 +19,6 @@ public interface CategoryRepository extends JpaRepository<Category, Long> {
 
     Category findById(Category category);
 
+    Page<Category> findAll(Pageable pageRequest);
 
 }
