042b08cc27a6a5e3313378b6e7125fc36cd65e75
Tomas.Fogwill <tomasfogwill@gmail.com>
2021-11-25 14:52:21 -0300

    changed method find all to receive int parameter and return pagedto

--- src/main/java/com/alkemy/ong/controller/CategoryController.java
+++ src/main/java/com/alkemy/ong/controller/CategoryController.java
@@ -1,11 +1,9 @@
 package com.alkemy.ong.controller;
 
-import java.util.List;
-
 import javax.validation.Valid;
 
 import com.alkemy.ong.dto.CategoryDto;
-import com.alkemy.ong.dto.CategoryDtoGetAll;
+import com.alkemy.ong.dto.CategoryPageDto;
 import com.alkemy.ong.exception.DataAlreadyExistException;
 import com.alkemy.ong.exception.NotFoundException;
 import com.alkemy.ong.model.Category;
@@ -27,32 +25,33 @@ public class CategoryController {
 
 	@Autowired
 	private CategoryService categoryService;
-	
-	@GetMapping
-	public ResponseEntity<List<CategoryDtoGetAll>> getAllCategories(){
-		return new ResponseEntity<>( categoryService.getAllCategories() , HttpStatus.OK );
+
+	@GetMapping(params = "page")
+	public ResponseEntity<CategoryPageDto> getAllCategories(@RequestParam int page) throws NotFoundException {
+		return new ResponseEntity<>(categoryService.getAllCategories(page), HttpStatus.OK);
 	}
 
 	@GetMapping("/{id}")
-	public ResponseEntity<Category>getById(@Valid @PathVariable long id)throws NotFoundException{
-		return new ResponseEntity<>(categoryService.getCategoryById(id),HttpStatus.OK); 
+	public ResponseEntity<Category> getById(@Valid @PathVariable long id) throws NotFoundException {
+		return new ResponseEntity<>(categoryService.getCategoryById(id), HttpStatus.OK);
 	}
 
 	@PostMapping("/create")
-    public ResponseEntity<Category> create(@Valid @RequestBody CategoryDto categoryDto) throws DataAlreadyExistException {
-        Category category = categoryService.save(categoryDto);
-        return new ResponseEntity<>(category, HttpStatus.CREATED);
-    }
+	public ResponseEntity<Category> create(@Valid @RequestBody CategoryDto categoryDto)
+			throws DataAlreadyExistException {
+		Category category = categoryService.save(categoryDto);
+		return new ResponseEntity<>(category, HttpStatus.CREATED);
+	}
 
-    @PutMapping("/{id}")
-    ResponseEntity<Category> updateCategory(@Valid @RequestBody CategoryDto categoryDto, @PathVariable Long id) throws NotFoundException {
-        return new ResponseEntity<>(categoryService.update(id, categoryDto), HttpStatus.OK);
-    }
+	@PutMapping("/{id}")
+	ResponseEntity<Category> updateCategory(@Valid @RequestBody CategoryDto categoryDto, @PathVariable Long id)
+			throws NotFoundException {
+		return new ResponseEntity<>(categoryService.update(id, categoryDto), HttpStatus.OK);
+	}
 
 	@DeleteMapping("/delete/{id}")
-	public ResponseEntity<?> delete(@PathVariable Long id) throws NotFoundException{
-			categoryService.deleteById(id);
-		return new ResponseEntity<>( HttpStatus.OK);
+	public ResponseEntity<?> delete(@PathVariable Long id) throws NotFoundException {
+		categoryService.deleteById(id);
+		return new ResponseEntity<>(HttpStatus.OK);
 	}
 }
-
