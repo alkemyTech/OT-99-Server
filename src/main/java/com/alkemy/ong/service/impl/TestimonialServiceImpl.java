7b0ac7a69bc11aa0ecb6359703b5448b94b7607d
Pablo Amado <pabloamado8@gmail.com>
2021-11-25 18:41:42 -0300

    OT99-83-TestimonialsPagination: format added and method getPage added

--- src/main/java/com/alkemy/ong/service/impl/TestimonialServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/TestimonialServiceImpl.java
@@ -1,5 +1,6 @@
 package com.alkemy.ong.service.impl;
 
+import com.alkemy.ong.dto.TestimonialDto;
 import com.alkemy.ong.dto.TestimonialRequest;
 import com.alkemy.ong.exception.DataAlreadyExistException;
 import com.alkemy.ong.exception.NotFoundException;
@@ -9,57 +10,85 @@ import com.alkemy.ong.repository.TestimonialRepository;
 import com.alkemy.ong.service.TestimonialService;
 import java.time.Instant;
 import java.util.Date;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TestimonialServiceImpl implements TestimonialService {
 
-    @Autowired
-    TestimonialMapper testimonialMapper;
-    @Autowired
-    TestimonialRepository testimonialRepository;
+	@Autowired
+	TestimonialMapper testimonialMapper;
 
-    @Override
-    public Testimonial save(TestimonialRequest testimonialRequest) throws DataAlreadyExistException {
+	@Autowired
+	TestimonialRepository testimonialRepository;
 
-        if ((testimonialRepository.findByName(testimonialRequest.getName()).isPresent())) {
-            throw new DataAlreadyExistException("This testimonial's name has already been used");
-        }
+	@Override
+	public Testimonial save(TestimonialRequest testimonialRequest) throws DataAlreadyExistException {
 
-        testimonialRequest.setLastUpdated(Date.from(Instant.now()));
-        Testimonial testimonial = testimonialMapper.dtoToEntity(testimonialRequest);
-        testimonial.setCreationDate(Date.from(Instant.now()));
-        return testimonialRepository.save(testimonial);
+		if ((testimonialRepository.findByName(testimonialRequest.getName()).isPresent())) {
 
-    }
+			throw new DataAlreadyExistException("This testimonial's name has already been used");
 
-    @Override
-    public Testimonial updateTestimonial(Long id, TestimonialRequest testimonialRequest) throws NotFoundException, DataAlreadyExistException {
+		}
 
-        if (!testimonialRepository.findById(id).isPresent()) {
+		Testimonial testimonial = testimonialMapper.dtoToEntity(testimonialRequest);
 
-            throw new NotFoundException("Testimonial could not be found");
+		testimonial.setLastUpdated(Date.from(Instant.now()));
+		
+		testimonial.setCreationDate(Date.from(Instant.now()));
 
-        }
-        if ((testimonialRepository.findByName(testimonialRequest.getName()).isPresent())) {
-            throw new DataAlreadyExistException("This testimonial's name has already been used");
-        }
+		return testimonialRepository.save(testimonial);
 
-        Testimonial testimonial = testimonialRepository.findById(id).get();
-        testimonialMapper.updateEntity(testimonialRequest, testimonial);
-        testimonial.setLastUpdated(Date.from(Instant.now()));
-        return testimonialRepository.save(testimonial);
+	}
 
-    }
+	@Override
+	public Testimonial updateTestimonial(Long id, TestimonialRequest testimonialRequest)
+			throws NotFoundException, DataAlreadyExistException {
 
-    @Override
-    public void delete(Long id) throws NotFoundException {
-        if (!testimonialRepository.existsById(id)) {
-            throw new NotFoundException("Testimonial could not be found");
-        }
-        
-        testimonialRepository.deleteById(id);
-    }
+		if (!testimonialRepository.findById(id).isPresent()) {
+
+			throw new NotFoundException("Testimonial could not be found");
+
+		}
+		if ((testimonialRepository.findByName(testimonialRequest.getName()).isPresent())) {
+
+			throw new DataAlreadyExistException("This testimonial's name has already been used");
+
+		}
+
+		Testimonial testimonial = testimonialRepository.findById(id).get();
+
+		testimonialMapper.updateEntity(testimonialRequest, testimonial);
+
+		testimonial.setLastUpdated(Date.from(Instant.now()));
+
+		return testimonialRepository.save(testimonial);
+
+	}
+
+	@Override
+	public void delete(Long id) throws NotFoundException {
+
+		if (!testimonialRepository.existsById(id)) {
+
+			throw new NotFoundException("Testimonial could not be found");
+		}
+
+		testimonialRepository.deleteById(id);
+	}
+
+	@Override
+	public List<TestimonialDto> getPage(Integer page) throws NotFoundException {
+
+		Pageable pageable = PageRequest.of(page, 10);
+		
+		Page<Testimonial> pageRecovered = testimonialRepository.findAll(pageable);
+
+		return testimonialMapper.toTestimonialDtoList(pageRecovered);
+	}
 
 }
